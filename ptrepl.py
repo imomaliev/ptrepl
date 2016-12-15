a84b574e86da63a54851344447e5907eaea6fbcd
Sardorbek Imomaliev <sardorbek.imomaliev@gmail.com>
2016-12-15 15:38:23 +0700

    bash commands usable with preceeding $ sign, deleted highlighting and completer for now

--- ptrepl.py
+++ ptrepl.py
@@ -4,12 +4,10 @@ import click
 
 from prompt_toolkit import prompt
 from prompt_toolkit.history import InMemoryHistory
-from prompt_toolkit.contrib.completers import SystemCompleter
-
-from pygments.lexers import BashLexer
 
 
 EXIT_COMMAND = 'exit'
+
 
 @click.command()
 @click.argument('command')
@@ -19,13 +17,15 @@ def main(command):
     while True:
         try:
             subcommand = prompt('{command} > '.format(command=command),
-                                lexer=BashLexer, completer=SystemCompleter(),
                                 history=history)
             if subcommand == EXIT_COMMAND:
                 break
-            subcommand = subcommand.split()
-            call_command = command.split()
-            call_command.extend(subcommand)
+            if subcommand[0] == '$':
+                call_command = subcommand[1:].split()
+            else:
+                subcommand = subcommand.split()
+                call_command = command.split()
+                call_command.extend(subcommand)
             subprocess.call(call_command)
         except EOFError:
             break  # Control-D pressed.
