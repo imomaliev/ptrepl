5597e2b0fa73a79d4919ad8118276a96f3397a35
Sardorbek Imomaliev <sardorbek.imomaliev@gmail.com>
2016-12-16 18:52:25 +0700

    fully working bash completion

--- ptrepl.py
+++ ptrepl.py
@@ -4,24 +4,34 @@ import click
 
 from prompt_toolkit import prompt
 from prompt_toolkit.history import InMemoryHistory
-from prompt_toolkit.contrib.completers import WordCompleter
+from prompt_toolkit.completion import Completer, Completion
 
 from bash_completion import get_completions
 
 EXIT_COMMAND = 'exit'
 
 
+class BashCompleter(Completer):
+    def __init__(self, command):
+        self.command = command
+
+    def get_completions(self, document, complete_event):
+        subcommand = document.text.rsplit(' ', 1)
+        start_position = -len(subcommand) if subcommand else 0
+        for completion in get_completions(' '.join([self.command, document.text]))[0]:
+            yield Completion(completion, start_position=start_position)
+
+
 @click.command()
 @click.argument('command')
 def main(command):
     history = InMemoryHistory()
-    completions = get_completions(command)
-    completer = WordCompleter(completions[0])
-
+    completer = BashCompleter(command)
     while True:
         try:
             subcommand = prompt('{command} > '.format(command=command),
-                                completer=completer, history=history)
+                                completer=completer, history=history,
+                                complete_while_typing=False)
             if subcommand == EXIT_COMMAND:
                 break
             if subcommand[0] == '$':
