5ee295602fdc803615ae2444660e0246fb456f36
Sardorbek Imomaliev <sardorbek.imomaliev@gmail.com>
2018-11-01 14:29:01 +0700

    fix for all repls having same history

--- ptrepl/history.py
+++ ptrepl/history.py
@@ -8,7 +8,7 @@ from .settings import settings
 def get_history(command):
     command = command.replace(' ', '').replace('/', '')
     if settings.LOCAL_SHADA:
-        history = Path(settings.LOCAL_SHADA_PATH, 'history')
+        history = Path(settings.LOCAL_SHADA_PATH, 'history/{}'.format(command))
     else:
         history = Path(XDG_DATA_HOME, 'ptrepl/history/{}'.format(command))
     if not history.exists():
