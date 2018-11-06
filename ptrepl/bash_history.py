fe8ffd0d30510cc8a991b2b5f8ec431acd126936
Sardorbek Imomaliev <sardorbek.imomaliev@gmail.com>
2018-11-06 18:02:09 +0700

    fix replacing push with non existent history

--- ptrepl/bash_history.py
+++ ptrepl/bash_history.py
@@ -108,5 +108,5 @@ def expand_history(command, history):
     try:
         command = command.replace('!!', history[-2])
     except IndexError:
-        raise BashHistoryIndexError()
+        pass
     return command, True
