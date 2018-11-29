f5ba1debc361dbcd1b8a8c026b82595f858f5a66
Sardorbek Imomaliev <sardorbek.imomaliev@gmail.com>
2018-11-29 17:13:19 +0700

    add travis badge

--- setup.py
+++ setup.py
@@ -17,13 +17,18 @@ def read(fname):
 setup(
     name='ptrepl',
     version='0.8.1',
+    author='Sardorbek Imomaliev',
     description='Run command as REPL-environment',
     long_description=read('README.md'),
-    author='Sardorbek Imomaliev',
+    long_description_content_type="text/markdown",
     url='https://github.com/imomaliev/ptrepl',
     license='MIT',
     packages=find_packages(exclude=['tests']),
     install_requires=['prompt_toolkit==2.0.7', 'click', 'pygments'],
     entry_points={'console_scripts': ['ptrepl = ptrepl.cli:main']},
-    classifiers=['Programming Language :: Python :: 3.6'],
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
 )
